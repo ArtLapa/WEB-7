27dc2aa8e8bf03f1b81ea779357ce3a159e79660
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-02-23 21:06:53 +0200

    Update additional_queries.py

--- WEB-7-main/WEB5/additional_queries.py
+++ WEB-7-main/WEB5/additional_queries.py
@@ -3,11 +3,18 @@ from sqlalchemy import create_engine, func, desc
 from sqlalchemy.orm import sessionmaker
 from models import Student, Grade, Subject, Teacher, Group
 
-engine = create_engine('postgresql://username:password@localhost:5432/your_database_name')
-
+engine = create_engine('postgresql://your_username:your_password@localhost:5432/your_database_name')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Ваші додаткові запити
+# Додаткові складні запити
+def additional_query_1():
+    # Середній бал, який певний викладач ставить певному студентові
+    pass
+
+def additional_query_2():
+    # Оцінки студентів у певній групі з певного предмета на останньому занятті
+    pass
 
 session.close()
+
